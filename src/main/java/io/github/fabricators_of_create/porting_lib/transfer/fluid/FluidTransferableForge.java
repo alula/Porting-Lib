37ca398a5f7917360de2cdbaddd4036a523a8981
alpha <alphamodeyt@gmail.com>
2022-05-27 12:59:28 -0500

    TransferUtilForge

---
+++ src/main/java/io/github/fabricators_of_create/porting_lib/transfer/fluid/FluidTransferableForge.java
@@ -0,0 +1,14 @@
+package io.github.fabricators_of_create.porting_lib.transfer.fluid;
+
+import javax.annotation.Nullable;
+
+import io.github.fabricators_of_create.porting_lib.util.LazyOptional;
+import net.minecraft.core.Direction;
+
+public interface FluidTransferableForge {
+	LazyOptional<IFluidHandler> getFluidHandler(@Nullable Direction direction);
+
+	default boolean shouldRunClientSide() {
+		return true;
+	}
+}

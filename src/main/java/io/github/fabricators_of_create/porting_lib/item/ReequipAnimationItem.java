8af81767a97dfeb52ba933c1f3bd1c0912325a10
alpha <alphamodeyt@gmail.com>
2022-05-27 18:41:01 -0500

    WalkOnSnowItem

--- src/main/java/io/github/fabricators_of_create/porting_lib/item/ReequipAnimationItem.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/item/ReequipAnimationItem.java
@@ -2,6 +2,7 @@ package io.github.fabricators_of_create.porting_lib.item;
 
 import net.minecraft.world.item.ItemStack;
 
+// TODO remove in favor of FabricItem#allowNbtUpdateAnimation?
 public interface ReequipAnimationItem {
 	default boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
 		return !oldStack.equals(newStack);
